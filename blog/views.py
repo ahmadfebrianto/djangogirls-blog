216325d569f2041f2e448eadee9c4024f13bbe31
Febri <achmadfebryanto@gmail.com>
2021-08-06 16:56:35 +0700

    Add edit and delete post functionality

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
 from .forms import PostForm
 from .models import Post
-from django.utils import timezone
 
 
 def index(request):
@@ -16,8 +15,8 @@ def post_detail(request, pk):
     return render(request, 'blog/post.html', {'post': post})
 
 
-def new_post(request):
-    if request.method == 'POST':
+def post_new(request):
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
@@ -28,3 +27,24 @@ def new_post(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog:post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect('blog:index')
