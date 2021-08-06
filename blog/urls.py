ee5ed403f47094cd3298d46e490dbe29ab87f143
Febri <achmadfebryanto@gmail.com>
2021-08-06 15:41:47 +0700

    Add form to create a post for admin

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from blog import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.new_post, name='new_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import PostForm
 from .models import Post
 from django.utils import timezone
 
 
 def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/index.html', {'posts': posts})
+    return render(request, 'blog/index.html', {'posts': posts, 'user': request.user})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post.html', {'post': post})
 
+
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog:post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
